d253d0033690c56ba4b75b8d96b72e9fcabc9bd3
rodce <vedkafinancial@gmail.com>
2025-11-04 22:04:15 -0600

    My first C#

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name : ");
+        string name = Console.ReadLine();
+        Console.Write(name);
+        Console.Write("What is your LastName : ");
+        string LastName= Console.ReadLine();
+        Console.Write($"Your Name is {LastName}, {name} {LastName}.");
     }
 }
