6864529df45b1d71b8a4c3d3cc45abbb12973321
rodce <vedkafinancial@gmail.com>
2025-11-08 18:58:19 -0600

    Finish

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -3,12 +3,56 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.Write("What is your first name : ");
-        string name = Console.ReadLine();
-        Console.Write(name);
-        Console.Write("What is your LastName : ");
-        string LastName= Console.ReadLine();
-        Console.Write($"Your Name is {LastName}, {name} {LastName}.");
+        
+        Console.Write("What is your grade percentage : ");
+        string grade = Console.ReadLine();
+        int number = int.Parse(grade);
+        string letter = "";
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+        else if (number >= 85)
+        {
+            letter = "B+";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 75)
+        {
+            letter = "C+";
+        }
+         else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        {
+            Console.WriteLine($"Your grade letter is : {letter}");
+        }
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulation you passed the course");
+        }
+        else
+        {
+            Console.WriteLine("Keep working hard, you will do it");
+        }
+
+           
+
     }
 }
+
+
