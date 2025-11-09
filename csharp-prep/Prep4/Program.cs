bb5d40669fd350801ca75afb3847d55dfb6239d8
rodce <vedkafinancial@gmail.com>
2025-11-09 17:03:32 -0600

    Finish

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,31 @@
 using System;
-
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 class Program
+
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> myFirstList = new List<int>();
+        int total = 0;
+        int aveg = 0;
+        int attempts = 0;
+        Console.Write("Please Enter Number : ");
+        string numbers = Console.ReadLine();
+        int numbersUser = int.Parse(numbers);
+        while (numbersUser != 0)
+        {
+            myFirstList.Add(numbersUser);
+            total = total + numbersUser;
+            attempts++;
+            Console.Write("Please Enter Number : ");
+            numbers = Console.ReadLine();
+            numbersUser = int.Parse(numbers);
+            if (numbersUser > )
+        }
+        Console.WriteLine("The sum is :" + " " + total);
+        aveg = (total / attempts);
+        Console.WriteLine("The Average is : "+" " + aveg);
+        
     }
 }
