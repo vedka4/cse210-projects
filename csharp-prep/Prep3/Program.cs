55eeb1e085e821612b19ca9abab6107bb1355efc
rodce <vedkafinancial@gmail.com>
2025-11-09 13:34:03 -0600

    GuessNumberComplted

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Data;
+using System.Globalization;
 
 class Program
+
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        const int minValue = 1;
+        const int maxValue = 10;
+
+        // computer give us the magic number
+        var rng = new Random();
+        int number = rng.Next(minValue, maxValue + 1);
+        Console.Write("What is your guess : ");
+        string guess = Console.ReadLine();
+        int attempts = 0;
+        int guessit = int.Parse(guess);
+        attempts++;
+        
+        while (guessit != number)
+        {
+            Console.WriteLine("Try again");
+            guess = Console.ReadLine();
+            guessit = int.Parse(guess);
+            attempts++;
+        }
+
+        Console.WriteLine("Good job" + "  " + guessit + " " + "Was the Magic Number");
+        Console.WriteLine("Attempts :" + " " + attempts);
+
     }
-}
+
+} 
